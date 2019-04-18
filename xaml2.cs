f6cfc577a24f0b6a035eb1cbcb5f3c82dbdd2493
Rafaela Medeiros <33203072+RafaelaMicaela@users.noreply.github.com>
2019-04-18 17:52:59 -0300

    Create xaml2.cs

---
+++ xaml2.cs
@@ -0,0 +1,13 @@
+TextBlock HorizontalAlignment="Left" Height="17" Margin="10,21,0,0" TextWrapping="Wrap" Text="km Rodados" VerticalAlignment="Top" Width="90"/>
+        <TextBlock HorizontalAlignment="Left" Height="31" Margin="10,67,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="77"><Run Text="Valor Pago"/><Run Text=" R$"/></TextBlock>
+        <TextBlock HorizontalAlignment="Left" Height="28" Margin="10,113,0,0" TextWrapping="Wrap" Text="Valor Litro R$" VerticalAlignment="Top" Width="90"/>
+        <TextBox x:Name="txtKmRodados"  HorizontalAlignment="Left" Height="32" Margin="110,21,-188,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="188"/>
+        <TextBox x:Name="txtValorPago" HorizontalAlignment="Left" Height="31" Margin="110,67,-188,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="188"/>
+        <TextBox x:Name="txtValorLitro" HorizontalAlignment="Left" Height="28" Margin="110,113,-188,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="188"/>
+        <Button Content="Calcular" HorizontalAlignment="Left" Height="30" Margin="110,160,-188,0" VerticalAlignment="Top" Width="188" Click="Button_Click"/>
+        <TextBlock HorizontalAlignment="Left" Height="29" Margin="10,224,0,0" TextWrapping="Wrap" Text="Km / Litro" VerticalAlignment="Top" Width="77"/>
+        <TextBlock HorizontalAlignment="Left" Height="27" Margin="10,264,0,0" TextWrapping="Wrap" Text="R$ / Km " VerticalAlignment="Top" Width="67"/>
+        <TextBox x:Name="txtKmLitro" HorizontalAlignment="Left" Height="29" Margin="110,224,-188,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="188"/>
+        <TextBox x:Name="txtMediaEmReais" HorizontalAlignment="Left" Height="27" Margin="110,264,-188,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="188"/>
+
+    </Grid>
